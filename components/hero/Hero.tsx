817c72cb0d07ef3598eeda4ff85ede4206fadd02
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-09-20 16:33:03 +0200

    added content for hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -3,7 +3,15 @@ import styles from "./hero.module.scss";
 function Hero() {
   return (
     <>
-      <section className={styles.hero}>Hero</section>
+      <section className={styles.hero}>
+        <h1>RLP Technologies</h1>
+        <h2>Where Your Dreams come true</h2>
+        <p>
+          Welcome to the home of some of the finest web Technologies you can
+          find in the entire Web! We provide YOU with the best available service
+          one can get. Stay and learn more about this services.
+        </p>
+      </section>
     </>
   );
 }
