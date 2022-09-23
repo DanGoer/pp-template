2baf1d9dbfb94057a74c0de5b3b1687efd106ce1
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-09-23 13:55:24 +0200

    changed backgorunds and styling in Info

--- components/info/Info.tsx
+++ components/info/Info.tsx
@@ -6,19 +6,27 @@ function Info() {
   const [activeInfo, setActiveInfo] = useState(0);
 
   return (
-    <section className={styles.info}>
-      <h1>{infoData[activeInfo].h1}</h1>
-      <p>{infoData[activeInfo].p}</p>
-      <div>
-        {infoButton.map((button, index) => {
-          return (
-            <button key={button.button} onClick={() => setActiveInfo(index)}>
-              {button.button}
-            </button>
-          );
-        })}
+    <section
+      className={`${styles.info__bg} ${styles[`info__info-${activeInfo}`]}`}
+    >
+      <div className={styles.info}>
+        <h1>{infoData[activeInfo].h1}</h1>
+        <p>{infoData[activeInfo].p}</p>
+        <div>
+          {infoButton.map((button, index) => {
+            return (
+              <button key={button.button} onClick={() => setActiveInfo(index)}>
+                {button.button}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
 }
 export default Info;
+
+{
+  /* @ts-ignore */
+}
